c5a4420114785f6f18865649d7711a425eccc4f1
Th. Androutsos <a8anassis@gmail.com>
2024-06-14 18:59:35 +0300

    notes and updates

--- browser/class-styling.js
+++ browser/class-styling.js
@@ -1,6 +1,6 @@
 const containerDOM = document.getElementById('container')
-const cls = ['container', 'text-center']
+//const cls = ['container', 'text-center']
 //containerDOM.classList.add('container', 'text-center')
 
-containerDOM.classList.add(...cls)
+containerDOM.classList.add('container', 'text-center')
 containerDOM.innerHTML = "<p>Hello CSS Styling through JS</p>"
