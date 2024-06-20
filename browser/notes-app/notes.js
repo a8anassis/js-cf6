d54c112efd1493f9d1547d50d65af79db3f31c70
Th. Androutsos <a8anassis@gmail.com>
2024-06-20 19:04:00 +0300

    notes

--- browser/notes-app/notes.js
+++ browser/notes-app/notes.js
@@ -1,17 +1,18 @@
 const daysGR = ['Κυριακή', 'Δευτέρα', 'Τρίτη', 'Τετάρτη', 'Πέμπτη', 'Παρασκευή', 'Σάββατο']
 const monthsGR = ['Ιανουαρίου', 'Φεβρουαρίου', 'Μαρτίου', 'Απριλίου', 'Μαΐου', 'Ιουνίου', 'Ιουλίου', 'Αυγούστου', 'Σεπτεμβρίου', 'Οκτωβρίου', 'Νοεμβρίου', 'Δεκεμβρίου']
+let noteId = 0
 
 window.addEventListener('DOMContentLoaded', function() {
 
     this.setInterval(printGRDate, 1000)
 
     this.document.querySelector('#addNoteBtn').addEventListener('click', function() {
-        onInsertController(document.querySelector('#inputNote').value.trim())
+        onInsertHandler(document.querySelector('#inputNote').value.trim())
     })
 
     this.document.querySelector('#inputNote').addEventListener('keyup', function(e) {
         if (e.key === 'Enter') {
-            onInsertController(this.value.trim())
+            onInsertHandler(this.value.trim())
         }
     })
 })
@@ -37,16 +38,52 @@ function printGRDate() {
 }
 
 /**
- * Controller for insert button clicked
+ * Handler for insert button clicked
  * or Enter pressed.
  * 
  * @param {string} data 
  */
-function onInsertController(data) {
+function onInsertHandler(data) {
     insertNote(data)
     reset()
 }
 
+function insertNote(note) {
+    if (!note) return
+
+    noteId++
+
+    let clone = document.querySelector('.note.hidden').cloneNode(true)
+    clone.classList.remove('hidden')
+
+    clone.querySelector('#noteCheck').addEventListener('click', function() {
+        strikeThrough(clone.querySelector('.note-text'))
+    })
+
+    clone.querySelector('.note_del-btn').addEventListener('click', function() {
+        deleteNote(this.parentNode)
+    })
+
+    let cloneNoteInfo = clone.querySelector('.note-info')
+    cloneNoteInfo.children[0].id = 'noteCheck' + noteId
+    cloneNoteInfo.children[1].htmlFor = 'noteCheck' + noteId
+    clone.children[1].id = 'noteDelBtn' + noteId
+
+    clone.querySelector('.note-text').innerHTML = note
+    document.querySelector('.notes-wrapper').appendChild(clone)
+}
+
+function strikeThrough(element) {
+    element.classList.toggle('line-through')
+}
+
+function deleteNote(note) {
+    note.remove()
+}
+
+function reset() {
+    document.querySelector('#inputNote').value = ''
+}
 
 
 
