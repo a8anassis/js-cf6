889e39250ef28ed97bef293d1f7dae9d88d08737
Th. Androutsos <a8anassis@gmail.com>
2024-06-20 20:43:02 +0300

    counter updates

--- browser/counter-app/counter.js
+++ browser/counter-app/counter.js
@@ -1,9 +1,10 @@
-let counter = 0
+const DEFAULT = 0
+let counter = DEFAULT
 
 window.addEventListener('DOMContentLoaded', function() {
-    this.document.querySelector('#btnDecr').addEventListener('click', () => onDecreaseClicked)
-    this.document.querySelector('#btnReset').addEventListener('click', () => onResetClicked)
-    this.document.querySelector('#btnIncr').addEventListener('click', () => onIncreaseClicked)
+    this.document.querySelector('#btnDecr').addEventListener('click', () => onDecreaseClicked())
+    this.document.querySelector('#btnReset').addEventListener('click', () => onResetClicked())
+    this.document.querySelector('#btnIncr').addEventListener('click', () => onIncreaseClicked())
 })
 
 
@@ -30,3 +31,61 @@ function onResetClicked() {
 function onIncreaseClicked() {
     increaseCounter()
 }
+
+// Model
+
+/**
+ * Decreases the counter by one and renders to UI.
+ */
+function decreaseCounter() {
+    counter--
+    //console.log(counter)
+    showCounter()
+}
+
+/**
+ * Resets the counter to zero and renders to UI.
+ */
+function resetCounter() {
+    counter = DEFAULT
+    showCounter()
+}
+
+/**
+ * Increases the counter by one and renders to UI.
+ */
+function increaseCounter() {
+    counter++
+    showCounter()
+}
+
+/**
+ * Assigns the counter to the corresponding UI Element.
+ * And gives styling accordingly.
+ */
+function showCounter() {
+    // Data binding
+    const counterDOM = document.querySelector('#counter')
+    counterDOM.textContent = counter
+    
+    // Styling
+    styleCounterElement(counterDOM)
+}
+
+function styleCounterElement(counterDOM) {
+
+    counterDOM.classList.toggle('color-green', counter > 0)
+    counterDOM.classList.toggle('color-red', counter < 0)
+    counterDOM.classList.toggle('color-black', counter === 0)
+
+    // if (counter == 0) {
+    //     counterDOM.style.color = "black"
+    // } else if (counter > 0 ) {
+    //     counterDOM.style.color = "green"
+    // } else { // if (counter < 0) {}
+    //     counterDOM.style.color = "red"
+    // }  
+}
+
+
+
